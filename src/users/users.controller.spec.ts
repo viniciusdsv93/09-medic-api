4d574c45afc95dec95ea1d7d840c43ea4bf09b14
Vinicius dos Santos Verissimo <viniciusdsv93@gmail.com>
2023-04-02 16:19:09 -0300

    test: should throw error if user not exists

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from "@nestjs/common";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
@@ -32,6 +33,20 @@ describe('Users Controller', () => {
       ]
       const result = await usersController.getUsers();
       expect(result).toEqual(mockedResult);
+    })
+
+
+    it('should return an user\'s data', async () => {
+      const { usersController } = makeSut();
+      const result = await usersController.getUserById('2');
+      expect(result).toEqual({ "email": "jorge@mail.com", "id": "2" });
+    })
+
+
+    it('should throw an error if user not exists', async () => {
+      const { usersController } = makeSut();
+      const promise = usersController.getUserById('5');
+      await expect(promise).rejects.toThrow();
     })
   })
 })
