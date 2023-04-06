a4a1d979aabb645b4e28bf60b6200f4234ce80b6
Vinicius dos Santos Verissimo <viniciusdsv93@gmail.com>
2023-04-06 20:15:18 -0300

    feat: make webscraping service iterate through each research detail and get data

--- src/web-scraping/web-scraping.service.ts
+++ src/web-scraping/web-scraping.service.ts
@@ -22,21 +22,24 @@ export class WebScrapingService {
     console.log(researchDetailArray)
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(`https://www.bing.com/news/search?q=chatgpt&qft=sortbydate%3d"1"`);
+    for (const r of researchDetailArray) {
+      await page.goto(`https://www.bing.com/news/search?q=${r.term}&qft=sortbydate%3d"1"`);
 
-    const links = await page.$$eval('.news-card-body a.title', links => {
-      return links.map(link => {
-        const description = link.parentElement.nextSibling;
-        console.log({description})
-        console.log({link})
-        return {
-          href: link.href,
-          text: link.textContent,
-          desc: description.textContent
-        }
+      const links = await page.$$eval('.news-card-body a.title', links => {
+        return links.map(link => {
+          const description = link.parentElement.nextSibling;
+          console.log({ description })
+          console.log({ link })
+          return {
+            href: link.href,
+            text: link.textContent,
+            desc: description.textContent
+          }
+        });
       });
-    });
 
-    console.log(links);
+      console.log(links);
+    }
+    await browser.close();
   }
 }
